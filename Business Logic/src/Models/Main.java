6b03028b96fb3658f5737095531075aeb47bb335
Sonia Narayanan <sonianarayanan@sonias-mbp.home>
2017-01-28 17:44:19 -0800

    Set Up Main class

---
+++ Business Logic/src/Models/Main.java
@@ -0,0 +1,30 @@
+package Models;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+
+public class Main {
+
+  public Main() {
+    // TODO Auto-generated constructor stub
+  }
+
+  /**
+   * Log-in Screen 
+   * @param primaryStage
+   */
+  public void start(final Stage primaryStage) {
+    primaryStage.setTitle("ThinkInc");
+  }
+  
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
