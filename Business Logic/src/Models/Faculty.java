fc78b8d3a70be0226e6ec55d66006c6e2fd68743
Kris Campos <kcampos0101@gmail.com>
2017-02-01 10:57:51 -0800

    Pulled FacultyPreferences into Faculty class

--- Business Logic/src/Models/Faculty.java
+++ Business Logic/src/Models/Faculty.java
@@ -1,7 +1,12 @@
 package Models;
 
 public class Faculty extends User {
-   private FacultyPreferences preferences;
+    private static final int DAYS_IN_WEEK = 7;
+    private static final int THIRTY_MIN_INTERVALS_PER_DAY = 48;
+
+    private int[][] preferredTimes = new int[DAYS_IN_WEEK][THIRTY_MIN_INTERVALS_PER_DAY];
+    // workload preference
+    // course preferences
 
     public Faculty(int userID, String userName, String email, String firstName, String lastName) {
         super(userID, userName, email, firstName, lastName);
--- Business Logic/src/Models/FacultyPreferences.java
+++
@@ -1,7 +0,0 @@
-package Models;
-
-/**
- * Created by Kris on 1/31/2017.
- */
-public class FacultyPreferences {
-}
