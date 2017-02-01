85e804966c330eadb9101728879564d7ecb8b667
Kris Campos <kcampos0101@gmail.com>
2017-02-01 09:45:08 -0800

    Added some models, created some utils.

--- Business Logic/src/Models/Faculty.java
+++ Business Logic/src/Models/Faculty.java
@@ -1,10 +1,13 @@
 package Models;
 
-public class Faculty extends User
-{
-   //private FacultyPreferences preferences;
-   
-   public void setPreferences()
+public class Faculty extends User {
+   private FacultyPreferences preferences;
+
+    public Faculty(int userID, String userName, String email, String firstName, String lastName) {
+        super(userID, userName, email, firstName, lastName);
+    }
+
+    public void setPreferences()
    {
       return;
    }
--- Business Logic/src/Models/User.java
+++ Business Logic/src/Models/User.java
@@ -1,32 +1,65 @@
 package Models;
 
-public class User
-{
-   private int userID;
-   private String userName;
-   private String hashWord;
-	
-   public boolean setID(int id)
-   {
-       //check for valid id, if not valid return false	
-	   userID = id;
-	   return true;
-   }
-	
-   public int getID()
-   {
-       return userID;
-   }
-	
-   public boolean setName(String name)
-   {
-      //check for valid string, return false if not valid
-      userName = name;
-      return true;
-   }
-   public String getName()
-   {
-      return userName;
-   }
-   
+/**
+ * Representation of a User as a model.
+ *
+ * @author Kris Campos
+ * @version 1 - initial version. 1/31/2017
+ */
+public class User {
+
+    private int userID;
+    private String userName;
+    private String email;
+    private String firstName;
+    private String lastName;
+
+    public User(int userID, String userName, String email, String firstName, String lastName) {
+        this.userID = userID;
+        this.userName = userName;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setFullName(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
