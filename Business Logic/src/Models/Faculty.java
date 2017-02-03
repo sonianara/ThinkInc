dad9c707d821cd01b8f1b75de6402eb27a4a7995
Landon Woollard <landonwoollard@gmail.com>
2017-02-03 10:13:28 -0800

    Small updates to faculty preferences, we need some way to know number of courses in department. Don't know if we pull from DB or keep a global variable. Also, we should set a hash function to go from course num (101-X) to index num (0-n)

--- Business Logic/src/Models/Faculty.java
+++ Business Logic/src/Models/Faculty.java
@@ -5,8 +5,8 @@ public class Faculty extends User {
     private static final int THIRTY_MIN_INTERVALS_PER_DAY = 48;
 
     private int[][] preferredTimes = new int[DAYS_IN_WEEK][THIRTY_MIN_INTERVALS_PER_DAY];
-    // workload preference
-    // course preferences
+    private int preferredTotalHours;// workload preference
+    private int[] coursePreferences = new int[NUM_COURSES];// course preferences -1, 0, 1 <=> CANNOT, CAN, PREFER
 
     public Faculty(int userID, String userName, String email, String firstName, String lastName) {
         super(userID, userName, email, firstName, lastName);
