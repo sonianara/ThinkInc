6929d2d0117994312449b28a941c9d40039cb919
Justin Foxhoven <djustinf@gmail.com>
2017-02-03 10:44:15 -0800

    will now parse both files; brittle though (shouldn't matter)

--- Business Logic/src/Analytics/Analyzer.java
+++ Business Logic/src/Analytics/Analyzer.java
@@ -1,8 +1,12 @@
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.util.IntSummaryStatistics;
 import java.util.Map;
 
 public class Analyzer {
@@ -10,23 +14,53 @@ public class Analyzer {
     /* Exception has to be handled by calling function. most likely cause would be
      * an inability to open the file (permission, existence, etc)
      */
-    public static Map<String, Integer> import_student_prefs(File csvFile) throws IOException{
-        Map<String, Integer> data = new HashMap<String, Integer>();
+    public static Map<Integer, Integer> import_student_prefs(File csvFile) throws IOException{
+        Map<Integer, Integer> data = new HashMap<Integer, Integer>();
         String nextLine;
         BufferedReader reader = new BufferedReader(new FileReader(csvFile));
 
         //begin reading
         reader.readLine(); //consume first line since it's useless (just a header, no data)
         while ((nextLine = reader.readLine()) != null) {
-            // We want index 6 [class name] and index 10 [seat demand]
+            // We want index 5 [class num], and index 10 [seat demand]
             String fields[] = nextLine.split(",");
             try {
-                data.put(fields[6], Integer.parseInt(fields[10]));
+                data.put(Integer.parseInt(fields[5]), Integer.parseInt(fields[10]));
             }
             catch (Exception e) { //ignore malformed input
                 continue;
             }
         }
         return data;
+    }
+
+    public static Map<String, Map<Integer, Integer>> import_cohort_data(File csvFile) throws IOException {
+        Map<String, Map<Integer, Integer>> data = new HashMap<String, Map<Integer, Integer>>();
+        data.put("Fall", new HashMap<Integer, Integer>());
+        data.put("Winter", new HashMap<Integer, Integer>());
+        data.put("Spring", new HashMap<Integer, Integer>());
+        String nextLine;
+        BufferedReader reader = new BufferedReader(new FileReader(csvFile));
+
+        //begin reading
+        for (int i = 0; i < 10; i++)
+            reader.readLine(); //consume header, not useful to us
+        while ((nextLine = reader.readLine()) != null) {
+            String fields[] = nextLine.split(",");
+            int classNum;
+            try {
+                classNum = Integer.parseInt(fields[0]); //navigates to the next class to grab data
+                //[1]->fall | [2]->winter | [3]->spring
+                fields = nextLine.split(",");
+                data.get("Fall").put(classNum, Integer.parseInt(fields[1]));
+                data.get("Winter").put(classNum, Integer.parseInt(fields[2]));
+                data.get("Spring").put(classNum, Integer.parseInt(fields[3]));
+            }
+            catch (Exception e) { //ignore malformed input
+                continue;
+            }
+        }
+        return data;
+
     }
 }
