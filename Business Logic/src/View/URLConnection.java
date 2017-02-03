9e4195b6572d65f5a8cb7b7b1e6e0d8141a6ab4f
Sonia Narayanan <sonianara96@gmail.com>
2017-02-03 10:12:03 -0800

    authentication

--- Business Logic/src/View/URLConnection.java
+++ Business Logic/src/View/URLConnection.java
@@ -1,46 +1,41 @@
 package View;
 import java.io.*; 
-import org.apache.http.*;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-
+import org.apache.commons.httpclient.*;
+import org.apache.commons.httpclient.methods.GetMethod;
 
 public class URLConnection {
   
   private static String url = "http://52.36.156.222:3000/user/signin";
   
   public static void main(String[] args) throws Exception{
-    CloseableHttpClient httpclient = HttpClients.createDefault();
-    try{
-        HttpGet httpGet = new HttpGet(url);
-        System.out.println("executing request " + httpGet.getURI());
-        
-        ResponseHandler<String> responseHandler = new ResponseHandler<String>(){
-        
-        public String handleResponse(final HttpResponse response) throws ClientProtocolException,IOException{
-          int status = response.getStatusLine().getStatusCode();
-          if (status >= 200 && status <300){
-            HttpEntity entity = response.getEntity();
-            return entity !=null ? EntityUtils.toString(entity) : null;
-          } 
-          else {
-            throw new ClientProtocolException("Unexpected response status: " + status);
-          }
-        }
-      };
-      String responseBody = httpclient.execute(httpGet,responseHandler);
-      System.out.println("-------------------------------------------");
-      System.out.println(responseBody);
-      System.out.println("-------------------------------------------");
-    }
-    finally{
-      httpclient.close();
-    }
+    HttpClient httpclient = new HttpClient();
+    GetMethod method = new GetMethod(url);
+    try {
+      // Execute the method.
+      int statusCode = httpclient.executeMethod(method);
+
+      if (statusCode != HttpStatus.SC_OK) {
+        System.err.println("Method failed: " + method.getStatusLine());
+      }
+
+      // Read the response body.
+      byte[] responseBody = method.getResponseBody();
+
+      // Deal with the response.
+      // Use caution: ensure correct character encoding and is not binary data
+      System.out.println(new String(responseBody));
+
+    } catch (HttpException e) {
+      System.err.println("Fatal protocol violation: " + e.getMessage());
+      e.printStackTrace();
+    } catch (IOException e) {
+      System.err.println("Fatal transport error: " + e.getMessage());
+      e.printStackTrace();
+    } finally {
+      // Release the connection.
+      method.releaseConnection();
+    }  
   }
 }
+
+
